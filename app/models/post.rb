5f376209f28ed5d0e9da8622724081edc7f43f2f
Mnkith <munkithabid@gmail.com>
2021-03-04 01:20:21 -0600

    commit tree 06082e1a6f304dc406f4963b8cf1113c4ff196d9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+  validate :title_is_not_clickbaity
+
+
+  def title_is_not_clickbaity
+    unless title&.match?(/\bWon't Believe\b|\bSecret\b|\bTop \d+\b|\bGuess\b/i) 
+      errors.add(:title, "this isn't appealing")
+    end
+  end
 end
